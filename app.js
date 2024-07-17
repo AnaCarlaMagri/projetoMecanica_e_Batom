12dae08adaf9d1694b1003698be6c6eb9cf27d09
ana.carla.magri@outlook.com <ana.carla.magri@outlook.com>
2024-07-17 01:02:27 -0300

    ajustando faq

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
-  if (faq.classList.contains("active")) {
-    faq.classList.remove("active");
-  } else {
-    faq.classList.add("active");
-  }
+  faq.addEventListener("click", () => {
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("active");
+    } else {
+      faq.classList.add("active");
+    }
+  });
 });
