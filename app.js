84b49c1186926c1b0747af9f16f7005752fdda1a
ana.carla.magri@outlook.com <ana.carla.magri@outlook.com>
2024-08-12 12:39:16 -0300

    finalizacao

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Script para FAQs
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
@@ -8,6 +10,19 @@ faqs.forEach((faq) => {
       faq.classList.add("active");
     }
   });
-}); 
+});
 
 
+
+function toggleContent(button) {
+  var card = button.parentElement;
+  var moreContent = card.querySelector(".more-content");
+
+  if (moreContent.style.display === "none") {
+    moreContent.style.display = "block";
+    button.textContent = "Mostrar Menos";
+  } else {
+    moreContent.style.display = "none";
+    button.textContent = "Continuar Lendo";
+  }
+}
